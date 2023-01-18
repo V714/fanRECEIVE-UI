bc6cf38e36b53a0429ec7f020bae3e0ca3b3fd66
Tomasz Lipczyński <vlipc@pop-os.localdomain>
2023-01-18 21:39:21 +0100

    reset password label

--- src/modals/ForgotPass.tsx
+++ src/modals/ForgotPass.tsx
@@ -31,7 +31,7 @@ function ForgotPass() {
           handleChange={(e) => setForgotEmail(e.value)}
         />
         <button className="buttonPrimary" type="submit" name="loginButton">
-          Login
+          Reset Password
         </button>
       </form>
     </>
